71e344083addda77ae438c0f7204736ea697df56
wk-j <somnuk.wk@gmail.com>
2018-08-21 00:33:46 +0700

    Client factory

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-## Http Client Performance
+## Http Client
 
 ```
-dotnet run --project src/Client.Perofrmance/Client.Performance.csproj
+dotnet-script scripts/New.csx
+netstat | grep waws-prod-bay
+
+dotnet-script scripts/Share.csx
+netstat | grep waws-prod-bay
+
+dotnet run --project src/ClientFactory/ClientFactory.csproj
 netstat | grep waws-prod-bay
 ```
---
+++ src/ClientFactory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClientFactory {
+    class Program {
+        static async Task Main(string[] args) {
+
+            var collection = new ServiceCollection();
+            collection.AddHttpClient();
+            collection.AddSingleton<MyService>();
+
+            var provider = collection.BuildServiceProvider();
+            var myService = provider.GetService(typeof(MyService)) as MyService;
+
+            for (int i = 0; i < 10; i++) {
+                var rs = await myService.GetGoogle();
+                Console.WriteLine(rs.Split('\n').Length);
+            }
+        }
+    }
+
+    class MyService {
+        IHttpClientFactory factory;
+        public MyService(IHttpClientFactory factory) {
+            this.factory = factory;
+        }
+
+        public async Task<string> GetGoogle() {
+            var client = factory.CreateClient();
+            return await client.GetStringAsync("http://aspnetmonsters.com");
+        }
+    }
+}
